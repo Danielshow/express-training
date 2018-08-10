80cd696d5642a700db8c4cad3be538cdc016d0e2
Daniel Show <danielshotonwa53@gmail.com>
2018-08-10 09:26:39 +0100

    Added my Express, Partails and other Docs for Learning

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const expressValidator = require('express-validator')
+const mongojs = require('mongojs');
+const db = mongojs('customerapp', ['users'])
+
+const app = express();
+
+//Set view Engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+//Body Parser middleware Documentation
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//Set Static Path to inpute our jquery, css, html pages
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Global variable
+app.use((req, res, next) => {
+  res.locals.errors = null;
+  next();
+})
+//Express Validator Middle middleware
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    let namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    }
+  }
+}));
+
+//End of Validator
+
+app.get('/', (req, res) => {
+  db.users.find((err, docs) =>{
+    // docs is an array of all the documents in mycollection
+    //console.log(docs);
+    res.render("index", {
+      title: "myCustomers",
+      users: docs,
+    });
+  })
+});
+
+app.post('/users/add', (req, res) => {
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('Email', 'Email is required').notEmpty();
+
+  let errors = req.validationErrors();
+  console.log(errors)
+
+  let newUser = {
+    name: req.body.name,
+    email: req.body.email
+  }
+  db.users.insert(newUser, (err, result)=>{
+    if (err){
+      console.log(err);
+    }
+    res.redirect('/')
+  })
+/*
+  if (errors) {
+    res.render("index", {
+      title: "myCustomers",
+      //users: users,
+      errors: errors
+    });
+  } else {
+    let newUser = {
+      name: req.body.name,
+      email: req.body.email
+    }
+//add to database
+    db.users.insert(newUser, (err, result)=>{
+      if (err){
+        console.log(err);
+      }
+      res.redirect('/')
+    })
+    console.log('SUCCESS')
+  }*/
+
+});
+
+app.listen(3000, () => {
+  console.log('Server started on Port 3000...');
+})
